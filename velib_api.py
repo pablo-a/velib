223ed8f2833420e7ab3dbdbd82b011aaa436897e
pablo-a <pabril@student.42.fr>
2017-07-05 12:12:15 +0200

    final change with method executemany for optimize sql inserts

--- velib_api.py
+++ velib_api.py
@@ -9,30 +9,26 @@ def fetch_velib_data():
         apiKey = f.readline()
 
     uri = "https://api.jcdecaux.com/vls/v1/stations?apiKey=9ea14072f364e9262198bdf89657952534d6531b&contract=Paris"
-    params = {
-        "apiKey" : apiKey,
-        "contract" : "Paris"
-    }
-    headers = {"Accept" : "application/json"}
     response = requests.get(uri)
     data = json.loads(response.content)
     return data
 
 def main():
     insert_query = """INSERT INTO velib
-    (id, name, address, latitude, longitude, banking, bonus, status, bike_stands,
-    available_bike_stands, available_bikes, last_update)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    (`number`, name, address, latitude, longitude, banking, bonus, status, bike_stands,
+    available_bike_stands, available_bikes, last_update, date_extract)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
     lst_params = []
 
     lst_station = fetch_velib_data()
 
     for item in lst_station:
-        update = strftime("%Y%m%d %H:%M", localtime(item['last_update'] / 1000))
+        update = strftime("%Y-%m-%d %H:%M", localtime(item['last_update'] / 1000))
+        today = strftime("%Y-%m-%d %H:%M")
         station = (item['number'], item['name'], item['address'],
         item['position']['lat'], item['position']['lng'], item['banking'], item['bonus'],
         item['status'], item['bike_stands'], item['available_bike_stands'],
-        item['available_bikes'], update)
+        item['available_bikes'], update, today)
         lst_params.append(station)
 
     #hardcore one :
@@ -44,7 +40,7 @@ def main():
     # for item in fetch_velib_data()]
 
     bdd = Pablo()
-    bdd.exec_req_with_args(insert_query, lst_params)
+    bdd.cursor.executemany(insert_query, lst_params)
     bdd.close()
 
 
