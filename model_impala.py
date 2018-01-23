92fa5588bdf6e0f9f70053756eddd99bc6a5ed2e
Pablo Abril <pabloabril75@gmail.com>
2018-01-23 12:51:56 +0100

    basic connexion through impala works

--- model_impala.py
+++ model_impala.py
@@ -1,8 +1,14 @@
 from impala.dbapi import connect
 
 def main():
-    conn = connect(host='my.impala.host', port=21050)
-    cur = conn.cursor()
+    bdd = connect(host='167.114.235.165', port=21050)
+    cursor = bdd.cursor()
+    cursor.execute("USE road_traffic_data")
+    cursor.execute('SHOW TABLES')
+    cursor.execute("SELECT * FROM velib_data LIMIT 100000")
+
+    for elem in cursor.fetchall():
+        print(elem)
 
 
 if __name__ == '__main__':
