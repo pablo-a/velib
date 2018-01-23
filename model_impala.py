29c59218c0e472de6b05abd1fa2256e7a0a747e8
Pablo Abril <pabloabril75@gmail.com>
2018-01-23 15:14:15 +0100

    add 2nd server solution.

--- model_impala.py
+++ model_impala.py
@@ -1,15 +1,38 @@
 from impala.dbapi import connect
+from flask_socketio import emit
+from datetime import datetime
+from json_serializer import json_dumps, json_loads
 
-def main():
-    bdd = connect(host='167.114.235.165', port=21050)
-    cursor = bdd.cursor()
+
+def sql_chunck_gene(bdd, chunk_size, start, end):
+    # create a cursor to execute the request.
+    cursor = bdd.cursor(dictify=True)
     cursor.execute("USE road_traffic_data")
-    cursor.execute('SHOW TABLES')
-    cursor.execute("SELECT * FROM velib_data LIMIT 100000")
-
-    for elem in cursor.fetchall():
+    cursor.execute("show tables")
+    for elem in cursor:
         print(elem)
+    print("Cursor created. Execute query on database.")
+    # TODO: remove limit 10000 in query
+    req = """SELECT latitude, longitude, available_bikes, date_extract
+    FROM velib_data ORDER BY date_extract LIMIT 1000000"""
+    params = (start, end)
+    cursor.execute(req, params)
+    print("Query on database is executed. Stream of data begins.")
 
+    # gonna fetch a fix number of results and yields them as a stream.
+    while True:
+        data = cursor.fetchmany(chunk_size)
+        if not data:
+            break
+        yield data
 
-if __name__ == '__main__':
-    main()
+def send_data_to_client(socketio, message):
+    eventlet.monkey_patch()
+    # Connect to the impala database when needed.
+    bdd = connect(host='167.114.235.165', port=21050)
+    print("Database connection is done")
+
+    current_data = []
+    for chunck in sql_chunck_gene(bdd, 1000, message['start'], message['end']):
+        emit("chunck_sent", json_dumps(chunck))
+        socketio.sleep(0.1)
--- velib_controller.py
+++ velib_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 import velib_model
+import model_impala
 
 
 app = Flask(__name__)
@@ -15,11 +16,12 @@ def index():
 @socketio.on('load_data')
 def feed_data(message):
     print("streaming & processing database")
-    velib_model.send_data_to_client(message)
+    velib_model.send_data_to_client(socketio, message)
+    # model_impala.send_data_to_client(socketio, message)
     print("streaming over")
 
 
 
 if __name__ == '__main__':
     print("Hello World")
-    socketio.run(app, host= '0.0.0.0')
+    socketio.run(app, debug=1, host= '0.0.0.0')
